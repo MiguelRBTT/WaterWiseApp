268a781a408d85e5c2196ebf3ffd7c9ff6f770ac
Miguel Buttendorf <miguelrbu@gmail.com>
2024-09-28 16:56:44 -0300

    Finished calculator.dart

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,81 +1,25 @@
 import 'package:flutter/material.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Icon(
-            Icons.keyboard_backspace,
-            color: Colors.white,
-          ),
-        ),
-        title: Text(
-          'Calculadora de Água',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF00A9D4),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => WaterCalculatorPage()),
-                );
-              },
-              child: Text('Calcular consumo de água',
-                  style: TextStyle(color: Colors.white)),
-            ),
-          ),
-        ),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(0xFF40FFDC),
-              Color(0xFF00A9D4),
-              Color(0xFF1C3166),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  _CalculatorState createState() => _CalculatorState();
 }
 
-class WaterCalculatorPage extends StatefulWidget {
-  @override
-  _WaterCalculatorPageState createState() => _WaterCalculatorPageState();
-}
-
-class _WaterCalculatorPageState extends State<WaterCalculatorPage> {
-  final _weightController = TextEditingController();
+class _CalculatorState extends State<Calculator> {
+  final TextEditingController _weightController = TextEditingController();
   String _result = '';
+  double?
+      qtd_drink; // Variável que irá armazenar a quantidade de água a ser bebida
 
   void _calculateWaterIntake() {
     final weight = double.tryParse(_weightController.text);
-    if (weight != null) {
-      final minWater = (weight * 35) / 1000; // em litros
-      final maxWater = (weight * 50) / 1000; // em litros
+    if (weight != null && weight > 0) {
+      qtd_drink = weight * 35; // Cálculo: 35 ml de água por kg de peso
       setState(() {
         _result =
-            'Você deve beber entre ${minWater.toStringAsFixed(2)} e ${maxWater.toStringAsFixed(2)} litros de água por dia.';
+            'Você deve beber ${qtd_drink!.toStringAsFixed(0)} ml de água por dia';
       });
     } else {
       setState(() {
@@ -93,62 +37,18 @@ class _WaterCalculatorPageState extends State<WaterCalculatorPage> {
           onTap: () {
             Navigator.pop(context);
           },
-          child: Icon(
+          child: const Icon(
             Icons.keyboard_backspace,
             color: Colors.white,
           ),
         ),
-        title: Text(
+        title: const Text(
           'Calculadora de Água',
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.transparent,
       ),
       body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: _weightController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Insira seu peso (kg)',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor:
-                      Color(0xFF00A9D4), // Botão com a cor principal do tema
-                ),
-                onPressed: _calculateWaterIntake,
-                child: Text('Calcular', style: TextStyle(color: Colors.white)),
-              ),
-              SizedBox(height: 20),
-              Text(
-                _result,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Color(0xFF1C3166),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF40FFDC),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/calendar');
-                },
-                child: Text('Continuar', style: TextStyle(color: Colors.white)),
-              ),
-            ],
-          ),
-        ),
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
@@ -158,6 +58,94 @@ class _WaterCalculatorPageState extends State<WaterCalculatorPage> {
               Color(0xFF00A9D4),
               Color(0xFF1C3166),
             ],
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Campo para inserir o peso
+                TextField(
+                  controller: _weightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Faça sua estimativa',
+                    labelStyle: TextStyle(color: Colors.white54),
+                    filled: true,
+                    fillColor: Colors.white24,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+                // Botão para calcular
+                ElevatedButton(
+                  onPressed: _calculateWaterIntake,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF00A9D4),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'Calcular',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Exibição do resultado
+                Text(
+                  _result,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Botão para ir para a página do calendário
+                ElevatedButton(
+                  onPressed: () {
+                    if (qtd_drink != null) {
+                      // Navegar para a rota do calendário, passando qtd_drink como argumento
+                      Navigator.pushNamed(
+                        context,
+                        '/calendar',
+                        arguments: qtd_drink, // Passar qtd_drink como argumento
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Primeiro faça o cálculo.'),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF00A9D4),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'Próxima Página',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
